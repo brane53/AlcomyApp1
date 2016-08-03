2ca4a7658a8bb090cfdd943ed780aeb0c220db6d
Brane Vrajich <brane53@gmail.com>
2016-08-02 23:08:17 -0600

    converted the home module and component.

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -1,85 +1,94 @@
-(function () {
+/// <reference path="../../../typings/index.d.ts" />
+/// <reference path="../user/shared/user.ts" />
+
+
+namespace alcomy {
 	'use strict';
-	
-	angular
-		.module('home')
-		.component('home', {
-			templateUrl: './app/components/home/home.component.html',
-			controller: HomeController,
-			bindings: {
-				$router: '<'
+	export namespace home {
+
+		let routeConfig: angular.RouteDefinition = [
+			{
+				path: '/dashboard/...',
+				name: 'Dashboard',
+				component: 'dashboard',
+				useAsDefault: true
 			},
-			$canActivate: $canActivate,
-			// ROUTES
-			$routeConfig: [
-				{
-					path: '/dashboard/...',
-					name: 'Dashboard',
-					component: 'dashboard',
-					useAsDefault: true
-				},
-				{
-					path: '/residents/...',
-					name: 'Residents',
-					component: 'residents'
-				},
-			]
-			
-		});
+			{
+				path: '/residents/...',
+				name: 'Residents',
+				component: 'residents'
+			},
+		]
 
 
-	$canActivate.$inject = ['$nextInstruction', '$prevInstruction', '$firebaseAuth', '$rootRouter'];
-	/* @ngInject */
-	function $canActivate($nextInstruction, $prevInstruction, $firebaseAuth, $rootRouter){
-		console.log('$canActivate: ', arguments);
-		
-		return $firebaseAuth().$requireSignIn()
-			.then(function(state){
-				console.log('You can access the home component');
-				return state;
-			})
-			.catch(function(err){
-				console.log('Auth Error: ', err);
-				$rootRouter.navigate(['Login']);
-			});
-	}
+		$canActivate.$inject = ['$nextInstruction', '$prevInstruction', '$firebaseAuth', '$rootRouter'];
+		/* @ngInject */
+		function $canActivate($nextInstruction, $prevInstruction, $firebaseAuth, $rootRouter) {
+			console.log('$canActivate: ', arguments);
 
-	HomeController.$inject = ['$mdSidenav', '$firebaseAuth', 'userService', ];
-	/* @ngInject */
-	function HomeController($mdSidenav, $firebaseAuth, userService) {
-		var vm = this;
-
-		// TODO Resolve that the current user was retrieved before loading the
-		// route
-		
-		vm.$routerOnActivate = function(){
-			
-			return userService.getCurrentUser()
-				.then(function(user){
-					vm.user = user;
+			return $firebaseAuth().$requireSignIn()
+				.then(function (state) {
+					console.log('You can access the home component');
+					return state;
 				})
-				.catch(function(err){
+				.catch(function (err) {
+					console.log('Auth Error: ', err);
+					$rootRouter.navigate(['Login']);
+				});
+		}
+
+		class HomeController {
+			static $inject = ['$mdSidenav', '$firebaseAuth', 'userService',];
+
+			/* @ngInject */
+			constructor(public $mdSidenav: angular.material.ISidenavService,
+									public $firebaseAuth,
+									public userService: alcomy.user.IUserService) { }
+
+			// TODO Resolve that the current user was retrieved before loading the
+			// route
+
+			$routerOnActivate() {
+
+				// return this.userService.getCurrentUser()
+				// 	.then(function (user) {
+				// 		this.user = user;
+				// 	})
+				// 	.catch(function (err) {
+				// 		console.error('Error MainToolbarController: ' + err);
+				// 	});
+			};
+
+
+
+			/*vm.$onInit = function(){
+				userService.getCurrentUser().then(function(user){
+					vm.user = user;
+				}).catch(function(err){
 					console.error('Error MainToolbarController: ' + err);
 				});
-		};
+			}*/
 
+			toggleMobileSidenav() {
+				this.$mdSidenav('mobile-sidenav').toggle();
+			}
 
-		
-		/*vm.$onInit = function(){
-			userService.getCurrentUser().then(function(user){
-				vm.user = user;
-			}).catch(function(err){
-				console.error('Error MainToolbarController: ' + err);
-			});
-		}*/
-
-		vm.toggleMobileSidenav = toggleMobileSidenav;
-
-		function toggleMobileSidenav(){
-			$mdSidenav('mobile-sidenav').toggle();
 		}
-		
+
+		angular
+			.module('home')
+			.component('home', {
+				templateUrl: './app/components/home/home.component.html',
+				controller: HomeController,
+				bindings: {
+					$router: '<'
+				},
+				$canActivate: $canActivate,
+				// ROUTES
+				$routeConfig: routeConfig
+
+			});
+
 	}
-	
-	
-})();
+
+};
--- app/components/home/home.module.ts
+++ app/components/home/home.module.ts
@@ -1,9 +1,10 @@
-(function () {
+/// <reference path="../../../typings/index.d.ts" />
+namespace alcomy {
 	'use strict';
-	
+	export namespace home {
 	angular
 		.module('home', [
 			'mainToolbar'
 		]);
-	
-})();
+}
+};
