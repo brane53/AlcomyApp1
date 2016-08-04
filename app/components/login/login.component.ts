4f60bf972f4e7a96e6f57a2dccc88326bf9e5b64
Brane Vrajich <brane53@gmail.com>
2016-08-03 22:41:57 -0600

    working on converting login component

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,159 +1,160 @@
 /// <reference path="../../../typings/index.d.ts" />
+/// <reference path="../user/shared/user.ts" />
 
 declare var firebase;
 
-(function () {
+namespace alcomy {
 	'use strict';
+	export namespace login {
 
-	angular
-		.module('login')
-		.component('login', {
-			templateUrl: './app/components/login/login.component.html',
-			controller: LoginController,
-			bindings: {
-				$router: '<'
-			}
-		});
+		class LoginController {
 
-	LoginController.$inject = ['$log', '$location', 'firebaseRoot', '$firebaseAuth', 'userService'];
-	/* @ngInject */
-	function LoginController($log, $location, firebaseRoot, $firebaseAuth, userService) {
-		var vm = this;
+			// Firebase Root
+			private fbRoot = firebase.database().ref();
 
-		// Firebase Root
-		var fbRoot = firebase.database().ref();
-		
-		// Properties
-		vm.newUserObj = {
-			firstName: null,
-			lastName: null,
-			email: null,
-			password: null
-		};
+			static $inject = ['$log', '$location', '$firebaseAuth', 'userService'];
+			newUserObj: Object;
+			isRegister: Boolean;
+			isLoggedIn: Boolean;
 
-		// TODO these need to be put into a service
-		vm.isRegister = null;  // TODO this needs to be implemented correctly
-		vm.isLoggedIn = null; //$firebaseAuth().isLoggedIn;    // TODO this needs to
-		// beimplemented correctly
-		
-		// Functions
-		vm.$onInit = $onInit();
-		vm.$routerOnActivate = $routerOnActivate;
-		vm.$routerCanDeactivate = $routerCanDeactivate;
-		vm.login = login;
-		vm.registerNewUser = registerNewUser;
-		vm.logout = logout;
-		vm.goToRegister = goToRegister;
-		
-		
-		function $onInit(){
-			console.log('url: ', $location.path());
-			if($location.path() === '/login'){
-				vm.isRegister = false;
-			} else if($location.path() === '/register'){
-				vm.isRegister = true;
+			/* @ngInject */
+			constructor(public $log: ng.ILogService,
+								public $location: ng.ILocationService,
+								public $firebaseAuth,
+								public userService) {
+
+				this.newUserObj = {
+					firstName: null,
+					lastName: null,
+					email: null,
+					password: null
+				};
+				this.isRegister = null;  // TODO this needs to be implemented correctly
+				this.isLoggedIn = null; //$firebaseAuth().isLoggedIn;    // TODO this needs to
 			}
 
-			console.log('$router: ', vm.$router);
-			console.log('isLoggedIn', $firebaseAuth().isLoggedIn);
-			vm.isLoggedIn = $firebaseAuth().isLoggedIn;
-			
-		}
-		
-		function $routerOnActivate(){
-			console.log('Auth State: ', $firebaseAuth().$waitForSignIn());
-			return $firebaseAuth().$waitForSignIn().then(function(state){
-				console.log('state: ', state);
-				vm.isLoggedIn = !!state;
-			});
-		}
 
-		function $routerCanDeactivate() {
-			return vm.isLoggedIn;
-		}
-		
-		// LOGIN
-		
-		// TODO make this more secure. implement safeguards
-		// TODO create route redirect if use is logged in
-		// Calls the login function from $firebaseAuth
-		function login(email, password) {
+			// TODO these need to be put into a service
 
-			$log.info('Email: ' + email + 'Password' + password);
-			
-			// $firebaseAuth().$signInWithEmailAndPassword(credentials)
-			$firebaseAuth().$signInWithEmailAndPassword(email, password)
-				.then(function(authData){
-					if(authData){
-						console.log('User id: ', authData.uid);
-						/*userService.setCurrentUser(authData.uid);*/
-						vm.isLoggedIn = true;
-						vm.$router.navigate(['Home']);
-					}
-				})
-				.catch(function(err){
-					console.warn('Error: ' + err);
+			// beimplemented correctly
+
+
+			$onInit() {
+				console.log('url: ', this.$location.path());
+				if (this.$location.path() === '/login') {
+					this.isRegister = false;
+				} else if (this.$location.path() === '/register') {
+					this.isRegister = true;
+				}
+
+				console.log('$router: ', $router);
+				console.log('isLoggedIn', this.$firebaseAuth().isLoggedIn);
+				this.isLoggedIn = this.$firebaseAuth().isLoggedIn;
+
+			}
+
+			$routerOnActivate() {
+				console.log('Auth State: ', this.$firebaseAuth().$waitForSignIn());
+				return this.$firebaseAuth().$waitForSignIn().then(state => {
+					console.log('state: ', state);
+					this.isLoggedIn = !!state;
 				});
-		}
-		
-		// REGISTER
-		function registerNewUser(newUserObj){
-			
-			// Credientials 
-			var credentials = {
-				email: newUserObj.email,
-				password: newUserObj.password
-			};
+			}
 
-			// Create user with given credentials
-			$firebaseAuth().$createUserWithEmailAndPassword(credentials)
-				.then(function(userData){
-					if(userData){
-						return $firebaseAuth().$signInWithEmailAndPassword(credentials);
-					}
-				})
-				.then(function(authData){
-					return fbRoot.child('users').child(authData.uid).set({
-						firstName: newUserObj.firstName,
-						lastName: newUserObj.lastName,
-						email: newUserObj.email
+			$routerCanDeactivate() {
+				return this.isLoggedIn;
+			}
+
+			// LOGIN
+
+			// TODO make this more secure. implement safeguards
+			// TODO create route redirect if use is logged in
+			// Calls the login function from $firebaseAuth
+			login(email, password) {
+
+				this.$log.info('Email: ' + email + 'Password' + password);
+
+				// $firebaseAuth().$signInWithEmailAndPassword(credentials)
+				this.$firebaseAuth().$signInWithEmailAndPassword(email, password)
+					.then(authData => {
+						if (authData) {
+							console.log('User id: ', authData.uid);
+							/*userService.setCurrentUser(authData.uid);*/
+							this.isLoggedIn = true;
+							this.$router.navigate(['Home']);
+						}
 					})
-					.then(function(data){
-						console.log('Data: ', data);
-						vm.isLoggedIn = true;
-						vm.$router.navigate(['Home']);
+					.catch(err => {
+						console.warn('Error: ' + err);
 					});
+			}
 
-				})
-				.catch(function(err){
-					console.warn("Error: " + err);
-				});
-		}
-		
-		// LOGOUT
-		/*FIX
-		* if you have multiple tabs open and the Home component is already
-		* loaded you can continue to navigate through the app once you logout.
-		*
-		* */
-		function logout(){
-			$firebaseAuth().$signOut();
-			vm.isRegister = true;
-			$firebaseAuth().$waitForSignIn()
-				.then(function(state){
-					vm.isLoggedIn = !!state;
-				});
+			// REGISTER
+			registerNewUser(newUserObj) {
+
+				// Credientials 
+				var credentials = {
+					email: newUserObj.email,
+					password: newUserObj.password
+				};
+
+				// Create user with given credentials
+				this.$firebaseAuth().$createUserWithEmailAndPassword(credentials)
+					.then(userData => {
+						if (userData) {
+							return this.$firebaseAuth().$signInWithEmailAndPassword(credentials);
+						}
+					})
+					.then(authData => {
+						return this.fbRoot.child('users').child(authData.uid).set({
+							firstName: newUserObj.firstName,
+							lastName: newUserObj.lastName,
+							email: newUserObj.email
+						})
+							.then(data => {
+								console.log('Data: ', data);
+								this.isLoggedIn = true;
+								this.$router.navigate(['Home']);
+							});
+
+					})
+					.catch(err => {
+						console.warn("Error: " + err);
+					});
+			}
+
+			// LOGOUT
+			/*FIX
+			* if you have multiple tabs open and the Home component is already
+			* loaded you can continue to navigate through the app once you logout.
+			*
+			* */
+			logout() {
+				this.$firebaseAuth().$signOut();
+				this.isRegister = true;
+				this.$firebaseAuth().$waitForSignIn()
+					.then(state => {
+						this.isLoggedIn = !!state;
+					});
+			}
+
+			goToRegister() {
+				this.$location.path('/register');
+				/*vm.$router.navigate(['Register']);*/
+				this.isRegister = true;
+			}
+
 		}
 
-		
-		
-		function goToRegister(){
-			$location.path('/register');
-			/*vm.$router.navigate(['Register']);*/
-			vm.isRegister = true;
-		}
-
+		angular
+			.module('login')
+			.component('login', {
+				templateUrl: './app/components/login/login.component.html',
+				controller: LoginController,
+				bindings: {
+					$router: '<'
+				}
+			});
 
 	}
-
-})();
+};
