684cc793d49c939a2698468debb51285c37801d4
Brane Vrajich <brane53@gmail.com>
2016-08-20 00:20:14 -0600

    working on the new resident dialog

--- app/components/residents/resident-list/resident-list.component.ts
+++ app/components/residents/resident-list/resident-list.component.ts
@@ -6,30 +6,56 @@ namespace alcomy {
 
 
 		class ResidentListController {
-			static $inject: Array<string> = ['residentsService'];
+			static $inject: Array<string> = ['$log', '$mdDialog', 'residentsService'];
 			residents: Array<any>;
 
 
 			/* @ngInject */
-			constructor(public residentsService) {
-				
-			 }
+			constructor(private $log, 
+									private $mdDialog: ng.material.IDialogService, 
+									private residentsService) {
 
-			$onInit(){
+			}
+
+			$onInit() {
 				this.residentsService.getResidents().then(residents => {
 					this.residents = residents;
 				});
 			}
-			
+
+			openResidentDialog(event) {
+				this.$mdDialog.show({
+					templateUrl: '../shared/new-resident-dialog/new-resident-dialog.html',
+					controller: function() {
+						let vm = this;
+						vm.resident = {
+							firstName: '',
+							lastName: '',
+							gender: '',
+							dateOfBirth: null
+						};
+
+						this.createNewResident = createNewResident;
+
+						function createNewResident(resident: Object){
+
+						}
+					},
+					controllerAs: '$ctrl',
+					parent: angular.element(document.body),
+					clickOutsideToClose: false
+				})
+			}
+
 
 		}
 
 		angular
-		.module('residents')
-		.component('residentList', {
-			templateUrl: './app/components/residents/resident-list/resident-list.component.html',
-			controller: ResidentListController,
-			transclude: false
-		});
-}
+			.module('residents')
+			.component('residentList', {
+				templateUrl: './app/components/residents/resident-list/resident-list.component.html',
+				controller: ResidentListController,
+				transclude: false
+			});
+	}
 };
